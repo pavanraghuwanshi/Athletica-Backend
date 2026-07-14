978f15a4b3a097784ec9947e35e9361aedf373a0
Pavan Raghuwanshi <pavanraghuwanshi77@gmail.com>
2026-07-14 14:27:57 +0530

    feat(cors): enable CORS for all routes

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 import { errorMiddleware } from './middlewares/error.middleware'
 import { notFoundMiddleware } from './middlewares/not-found.middleware'
 import { registerRoutes } from './routes'
 
 const app = new Hono()
+
+app.use('*', cors({ origin: '*' }))
 
 registerRoutes(app)
 
